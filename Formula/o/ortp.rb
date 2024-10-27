baed1d028325d6f069af9aed808bc2f20719b562
ellie is not coding in the slightest <166862894+eleanordoesntcode@users.noreply.github.com>
2024-10-27 18:25:44 +0100

    ortp: update bctoolbox

--- Formula/o/ortp.rb
+++ Formula/o/ortp.rb
@@ -12,8 +12,8 @@ class Ortp < Formula
     # bctoolbox appears to follow ortp's version. This can be verified at the GitHub mirror:
     # https://github.com/BelledonneCommunications/bctoolbox
     resource "bctoolbox" do
-      url "https://gitlab.linphone.org/BC/public/bctoolbox/-/archive/5.3.84/bctoolbox-5.3.84.tar.bz2"
-      sha256 "1c9ec26a91e74f720b16416a0e9e5e84c8aeb04b3fc490c22a4b2fc10ab6d5e3"
+      url "https://gitlab.linphone.org/BC/public/bctoolbox/-/archive/5.3.93/bctoolbox-5.3.93.tar.bz2"
+      sha256 "f2e5436ae48a745563dabbf4884355a2f1362c03405f0e4f21a8b6fb20fbeb12"
     end
   end
 
